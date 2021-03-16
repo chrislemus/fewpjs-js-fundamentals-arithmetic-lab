8fce915e7d2305344c4873aa25cba4169881a21d
chrislemus <icrislemus@gmail.com>
2021-03-16 17:34:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 
 // Write your code here
+const [num1, num2] = [2, 31]
+const multiply = num1 * num2
+const random = parseInt(Math.random() * 100);
+const num3 = 104
+const num4 = 100
+const mod = num3 % num4;
+const max = Math.max(2,8,20)
